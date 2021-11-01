c687e44ee461f7ba2b455d28cb776491e389fa86
Eliss-good <elisssupergames@gmail.com>
2021-11-01 13:44:36 +0300

    test Hash

---
+++ Pracktikprogram/HASH/HASH.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+const int size_alphabet = 62;
+const int size_string = 6;
+const unsigned long long int  kolvo_itertion = create
+
+class Hash
+{
+	private:
+
+		long long int*;
+
+};
+
+void creat_hash(std::string slovo)
+{
+	std::string n_word;
+
+	while()
+
+		unsigned long long int hash = 0;
+		while()
+		hash *= size_alphabet;
+	    if(a >= '0' && a <= '9'){
+	        hash += static_cast<int>(a) - '0';
+	    } else if(a >= 'A' && a <= 'Z'){
+	        hash += static_cast<int>(a) - 'A' + 10;
+	    } else if(a >= 'a' && a <= 'z'){
+	        hash += static_cast<int>(a) - 'a' + 36;
+	    }
+
+
+	}
+
+
+}
+
+std::string fun_hash(unsigned long long int& hesh) //на основе этой функции можем расхешировать и получить мтроку
+{
+	std::string n_word;
+
+
+    long long int max = 1;
+    char p;
+    long long int k;
+    while(max <= hash){
+    max *= size_alphabet;
+    }
+    max /= size_alphabet;
+
+    while (max > 0)
+    {  
+        long long int m1 = desit / max;
+        hash = hahs - m1*max;
+
+        if((k <= 9))
+        {
+            p = '0' + k;
+        } 
+        else if (k >= 10 && k < 36) 
+        {
+            p = 'A' + k - 10;
+        } 
+        else if(k >= 36 && k < 62) 
+        {
+            p = 'a' + k - 36;
+        }
+        
+        n_word.push_back(p);
+
+        max /= size_alphabet;
+    }
+
+    return n_word;
+} 
+
+void unical_string()
+{
+	for(int i =  0; i < size_string; ++i)
+	{
+
+	}
+
+	for(unsigned long long int = 0; i < kolvo_itertion; ++i)
+	{
+
+	}
+}
+
+
+int main()
+{
+	std::string maxstr;
+	for(int i = 0; i < size_string; ++i)
+	{
+		maxstr.push_back('z');
+	}
+
+
+
+
+
+}
