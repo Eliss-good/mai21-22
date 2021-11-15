1f3441cdc5e18501975d2125ba82fc98b2ba3a10
Eliss-good <elisssupergames@gmail.com>
2021-11-15 10:45:24 +0300

    FIX5: add ptr_connect

--- Pracktikprogram/cash1/cash_dif.cpp
+++ Pracktikprogram/cash1/cash_dif.cpp
@@ -3,153 +3,197 @@
 #include <exception>
 
 template <typename T>
-class item{
-	public:
+class item {
+public:
+	item* next_ptr;
+	item* back_ptr;
+	T data;
+	unsigned int read;
+};
 
-	 	item* next_ptr;
-		item* back_ptr;
-		T data;
-		unsigned int read;
+
+template <typename T>
+class List
+{
+private:
+	item<T>* glavptr = nullptr;
+
+	item<T>* poisk(int position);
+	void change(item<T>* u1, item<T>* u2);
+	void sort(int left, int right);
+
+public:
+	void additem(T n_element);
+	void get_item(int position);
+
+	void all_list();
+	int check_size();
+
+	~List();
 };
 
 template <typename T>
-int check_size(item<T>* arr_obj_list)
+int List<T>::check_size()
 {
-	int i = 0; 
-	item<T>* chaeck = arr_obj_list[i].next_ptr;
+	item<T>* cur = glavptr;
+	int i = 0;
 
-	while(chaeck != nullptr)
+	while (cur != nullptr)
 	{
 		++i;
-		item<T>* chaeck = arr_obj_list[i].next_ptr;
+		cur = cur->next_ptr;
 	}
 
 	return i;
 }
 
 template <typename T>
-void connect_fun(int size_arr, item<T>* arr_obj_list)
+void List<T>::additem(T n_element)
 {
-	for(int i = 0; i < size_arr; ++i)
+	item<T>* n_item = new item<T>;
+
+	if (glavptr == nullptr)
 	{
-		if(i == 0)
-		{
-			arr_obj_list[i].next_ptr = &arr_obj_list[i+1]; 
-			arr_obj_list[i].back_ptr = nullptr;
-		} 
-		else if(i == size_arr - 1)
-		{
-			arr_obj_list[i].next_ptr = nullptr;
-			arr_obj_list[i].back_ptr = &arr_obj_list[i-1];
-		}
-		else
-		{
-			arr_obj_list[i].next_ptr = &arr_obj_list[i+1];
-			arr_obj_list[i].back_ptr = &arr_obj_list[i-1];
-		}
+		n_item->next_ptr = nullptr;
+		n_item->back_ptr = nullptr;
+
+		glavptr = n_item;
 	}
+	else
+	{
+		item<T>* cur = glavptr;
+		while (cur->next_ptr != nullptr)
+		{
+			cur = cur->next_ptr;
+		}
+		n_item->next_ptr = nullptr;
+		n_item->back_ptr = cur;
+
+		cur->next_ptr = n_item;
+	}
+
+	n_item->data = n_element;
+	n_item->read = 0;
 }
 
 template <typename T>
-void change(item*<T> arr_obj_list, unsigned int& max, unsigned int& min)
+item<T>* List<T>::poisk(int position)
 {
-	item<T> tet;
-	tet.next_ptr = arr_obj_list[max].next_ptr;
-	tet.back_ptr = arr_obj_list[max].back_ptr;
-	tet.data = arr_obj_list[max].data;
-	tet.read = arr_obj_list[max].read;
+	item<T>* cur = glavptr;
+	while (cur->next_ptr != nullptr && position - 1 > 0)
+	{
+		cur = cur->next_ptr;
+		position--;
+	}
 
-	arr_obj_list[max].next_ptr = arr_obj_list[min].next_ptr;
-	arr_obj_list[max].back_ptr = arr_obj_list[min].back_ptr;
-	arr_obj_list[max].data = arr_obj_list[min].data;
-	arr_obj_list[max].read = arr_obj_list[min].read;
-
-	arr_obj_list[min].next_ptr = tet.next_ptr;
-	arr_obj_list[min].back_ptr = tet.back_ptr;
-	arr_obj_list[min].data = tet.data;
-	arr_obj_list[min].read = tet.read;
+	return cur;
 }
 
 template <typename T>
-void sort(item<T>* arr_obj_list, int left, int right)
+void List<T>::change(item<T>* u1, item<T>* u2)
+{
+	T pdata = u1->data;
+	unsigned int appeals = u1->read;
+
+	u1->data = u2->data;
+	u1->read = u2->read;
+
+	u2->data = pdata;
+	u2->read = appeals;
+}
+
+template <typename T>
+void List<T>::sort(int left, int right)
 {
 	int l_hold = left;
 	int r_hold = right;
-	int pivot = arr_obj_list[left].read;
+	int pivot = poisk(left)->read;
 
 	while (left < right) {
 
-		while ((arr_obj_list[right].read <= pivot) && (left < right)) right--;
+		while ((poisk(right)->read <= pivot) && (left < right)) right--;
 
 		if (left != right)
 		{
-			change(arr_obj_list[right].read, arr_obj_list[left].read);
+			change(poisk(right), poisk(left));
 			left++;
 		}
 
-		while ((arr_obj_list[left].read) && (left < right)) left++;
+		while ((poisk(left)->read) && (left < right)) left++;
 
 		if (left != right)
 		{
-			change(arr_obj_list[left].read, arr_obj_list[right].read);
+			change(poisk(left), poisk(right));
 			right--;
 		}
 	}
 
-	read[left] = pivot;
-	if (l_hold < right) sort(item<T>* arr_obj_list,l_hold, right - 1);
-	if (r_hold > right) sort(item<T>* arr_obj_list,right + 1, r_hold);
+	poisk(left)->read = pivot;
+	if (l_hold < right) sort(l_hold, right - 1);
+	if (r_hold > right) sort(right + 1, r_hold);
 
 }
 
 template <typename T>
-void get_item(int& position,item*<T> arr_obj_list)
+void List<T>::get_item(int position)
 {
-	arr_obj_list[position].read += 1;
-	std::cout << '\t' << arr_obj_list[position - 1].data << " number of requests: " << arr_obj_list[position - 1].read << '\n';
-
-	sort(arr_obj_list, 0 , check_size(arr_obj_list));
-	connect_fun(check_size(arr_obj_list),arr_obj_list);
-}
-
-
-template <typename T>
-void all_list(item<T>* arr_obj_list)
-{
-	int i = check_size(arr_obj_list); 
-
-	for(int k = 0; k <= i; k++)
+	item<T>* cur = glavptr;
+	while (cur->next_ptr != nullptr && position - 1 > 0)
 	{
-		std::cout << "#" << k << '\t' << arr_obj_list[k].data << " " << " number of requests: " << arr_obj_list[k].read << '\n';
+		cur = cur->next_ptr;
+		position--;
+	}
+
+	std::cout << '\t' << cur->data << " number of requests: " << cur->read << '\n';
+
+	cur->read += 1;
+	sort(0, check_size());
+}
+
+
+template <typename T>
+void List<T>::all_list()
+{
+	item<T>* cur = glavptr;
+
+	while (cur != nullptr)
+	{
+		std::cout << "#" << '\t' << cur->data << " " << " number of requests: " << cur->read << '\n';
+		cur = cur->next_ptr;
 	}
 }
 
 template <typename T>
-void filling_arr(int& size, item<T>* arr_obj_list)
+List<T>::~List()
 {
-	for(int i = 0; i < size; ++i)
-	{
-		std::cin >> arr_obj_list[i].data;
+	if (glavptr == nullptr) return;
 
-		arr_obj_list[i].read = 0;
+	item<T>* cur = glavptr;
+	while (cur->next_ptr != nullptr)
+	{
+		cur = cur->next_ptr;
+		delete[] cur->back_ptr;
 	}
 
-	connect_fun(size, arr_obj_list);
+	delete[] cur;
 }
 
 int main()
 {
-	int size_arr = 8;
+	List<std::string> obj;
+	obj.additem("koks");
+	obj.additem("mops");
+	obj.additem("anans");
+	obj.additem("platina");
+	obj.additem("buda");
 
-	item<std::string>* arr_obj_list = new item<std::string>[size_arr];
-	filling_arr(size_arr, arr_obj_list);
+	obj.get_item(1);
+	obj.get_item(1);
+	obj.get_item(2);
+	obj.get_item(1);
+	obj.get_item(3);
 
-	get_item(3, arr_obj_list);
-	get_item(5, arr_obj_list);
-	get_item(5, arr_obj_list);
-	get_item(5, arr_obj_list);
+	obj.all_list();
 
-	all_list(arr_obj_list);
-
+	return 0;
 }
-
